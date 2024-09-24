8ab75f5e0258ff719599120cf9b2a9af29a4f692
Vincent Acanfora <brodino96@gmail.com>
2024-09-24 15:28:14 +0200

    Replaced playerLoaded method

--- client/modules/criticalHits.lua
+++ client/modules/criticalHits.lua
@@ -5,13 +5,5 @@ end
 RegisterNetEvent("br_framework:onPedChange")
 AddEventHandler("br_framework:onPedChange", removeCriticalHits)
 
-if Framework == "esx" then
-    RegisterNetEvent("esx:playerLoaded")
-    AddEventHandler("esx:playerLoaded", removeCriticalHits)
-elseif Framework == "qb" then
-    RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
-    AddEventHandler("QBCore:Client:OnPlayerLoaded", removeCriticalHits)
-elseif Framework == "ox" then
-    RegisterNetEvent("ox:playerLoaded")
-    AddEventHandler("ox:playerLoaded", removeCriticalHits)
-end
+RegisterNetEvent("br_utils:playerLoaded")
+AddEventHandler("br_utils:playerLoaded", removeCriticalHits)
