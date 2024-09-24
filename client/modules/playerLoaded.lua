7660bf17e984d4f573549560564360ff7c235685
Vincent Acanfora <brodino96@gmail.com>
2024-09-24 15:28:29 +0200

    Created "br_utils:playerLoaded" event

---
+++ client/modules/playerLoaded.lua
@@ -0,0 +1,14 @@
+local function playerLoaded()
+    TriggerEvent("br_utils:playerLoaded")
+end
+
+if Framework == "esx" then
+    RegisterNetEvent("esx:playerLoaded")
+    AddEventHandler("esx:playerLoaded", playerLoaded)
+elseif Framework == "qb" then
+    RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
+    AddEventHandler("QBCore:Client:OnPlayerLoaded", playerLoaded)
+elseif Framework == "ox" then
+    RegisterNetEvent("ox:playerLoaded")
+    AddEventHandler("ox:playerLoaded", playerLoaded)
+end
