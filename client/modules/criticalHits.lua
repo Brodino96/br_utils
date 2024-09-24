9a924037baf1527db41113cec426d13b7667447b
Vincent Acanfora <brodino96@gmail.com>
2024-09-24 18:11:36 +0200

    Added disableHatDrop and disableAutmaticHelmet, unified all functions to be under "pedChanged()"

--- client/modules/criticalHits.lua
+++ client/modules/criticalHits.lua
@@ -1,9 +1,12 @@
-local function removeCriticalHits()
-    SetPedSuffersCriticalHits(PlayerPedId(), (not Config.disableHeadshot))
+local function pedChanged()
+    local playerPed = PlayerPedId()
+    SetPedSuffersCriticalHits(playerPed, (not Config.player.disableHeadshot))
+    SetPedCanLosePropsOnDamage(playerPed, (not Config.player.disableHatDrop), 0)
+    SetPedConfigFlag(playerPed, 35, (not Config.player.disableAutomaticHelmet))
 end
 
 RegisterNetEvent("br_utils:onPedChange")
-AddEventHandler("br_utils:onPedChange", removeCriticalHits)
+AddEventHandler("br_utils:onPedChange", pedChanged)
 
 RegisterNetEvent("br_utils:playerLoaded")
-AddEventHandler("br_utils:playerLoaded", removeCriticalHits)
+AddEventHandler("br_utils:playerLoaded", pedChanged)
