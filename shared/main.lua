9835bb4005a58b352bd7d37805832695cf6f8c50
Vincent Acanfora <brodino96@gmail.com>
2024-09-24 15:07:24 +0200

    Created

---
+++ shared/main.lua
@@ -0,0 +1,11 @@
+local function getFramework()
+    if GetResourceState("es_extended") == "started" then
+        return "esx"
+    elseif GetResourceState("qb-core") == "started" then
+        return "qb"
+    elseif GetResourceState("ox_core") == "started" then
+        return "ox"
+    end
+end
+
+Framework = getFramework()
