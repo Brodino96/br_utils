d7451810d649ed74400f82d353ff83696d81e982
Vincent Acanfora <brodino96@gmail.com>
2024-09-24 18:10:31 +0200

    Created a player section to house all the flags on the playerPed, added disableHatDrop and disableAutmaticHelmet

--- config.lua
+++ config.lua
@@ -1,3 +1,7 @@
 Config = {
-    disableHeadshot = true --
+    player = {
+        disableHeadshot = true, -- Stops the ped from taking more damage when headshotted
+        disableHatDrop = true, -- Stops the ped from losing his hat when he gets damaged
+        disableAutomaticHelmet = true, -- Stops the ped from automatically wearing an helmet while on a motorbikes
+    }
 }
