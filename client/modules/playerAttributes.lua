5bf034ef1a33a3494c4911698ce681b2a627b7b5
Vincent Acanfora <brodino96@gmail.com>
2024-10-14 14:47:50 +0200

    Added "disableIdleCamera" function

--- client/modules/playerAttributes.lua
+++ client/modules/playerAttributes.lua
@@ -3,6 +3,16 @@ local function setAttributes()
     SetPedSuffersCriticalHits(playerPed, (not Config.player.disableHeadshot))
     SetPedCanLosePropsOnDamage(playerPed, (not Config.player.disableHatDrop), 0)
     SetPedConfigFlag(playerPed, 35, (not Config.player.disableAutomaticHelmet))
+
+    if Config.player.disableIdleCamera then
+        CreateThread(function ()
+            while true do
+                InvalidateIdleCam()
+                InvalidateVehicleIdleCam()
+                Wait(25000)
+            end
+        end)
+    end
 end
 
 RegisterNetEvent("br_utils:onPedChange")
--- config.lua
+++ config.lua
@@ -6,6 +6,7 @@ Config = {
         disableHeadshot = true, -- Stops the playerPed from reciving critical damage on an headshot (it will apply the same damage as a bodyshot)
         disableHatDrop = true, -- Stops the playerPed from losing his hat when he gets damaged by melee
         disableAutomaticHelmet = true, -- Stops the playerPed from automatically wearing an helmet while on bikes or bicycles
+        disableIdleCamera = true, -- Stops the game from enabling the idle camera when standing still for 60 seconds
     },
 
     weaponDamage = { -- If active the script will override the current weapon damage with the ones in the values array
