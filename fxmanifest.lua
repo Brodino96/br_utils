3e3f50e416511a7643383f1959c8c20339f2ede7
Vincent Acanfora <brodino96@gmail.com>
2024-10-14 14:35:49 +0200

    Added "debug.lua" and "weaponDamage.lua", placed "config.lua" as the first resource to start

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,13 +7,15 @@ author "Brodino"
 description "A bunch of utilities"
 
 shared_scripts {
-    "shared/framework.lua",
     "config.lua",
+    "shared/debug.lua",
+    "shared/framework.lua",
 }
 
 client_scripts {
     "client/main.lua",
     "client/modules/playerLoaded.lua",
     "client/modules/onPedChange.lua",
-    "client/modules/criticalHits.lua",
+    "client/modules/playerAttributes.lua",
+    "client/modules/weaponDamage.lua"
 }
