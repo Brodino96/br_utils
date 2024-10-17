c472083f445d85afa1aeb569dcee721541c5bddc
Vincent Acanfora <brodino96@gmail.com>
2024-10-17 17:19:46 +0200

    Created

---
+++ client/modules/onVehicleChange.lua
@@ -0,0 +1,15 @@
+LastVehicle = GetVehiclePedIsIn(PlayerPed, false)
+
+CreateThread(function ()
+    while true do
+        Wait(0)
+
+        local currentVeh = GetVehiclePedIsIn(PlayerPed, false)
+        if currentVeh == LastVehicle then
+            goto skip
+        end
+        TriggerEvent("br_utils:onVehicleChange")
+        LastVehicle = currentVeh
+        ::skip::
+    end
+end)
