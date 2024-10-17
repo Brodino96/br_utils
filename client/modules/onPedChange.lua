22895918b25f3fe4b8efc9762786dc429fb3db6a
Vincent Acanfora <brodino96@gmail.com>
2024-10-17 17:19:39 +0200

    Updated to save the current ped as a global variable

--- client/modules/onPedChange.lua
+++ client/modules/onPedChange.lua
@@ -1,14 +1,14 @@
-local playerPed = PlayerPedId()
+PlayerPed = PlayerPedId()
 
 CreateThread(function ()
     while true do
         Wait(0)
         local currentPed = PlayerPedId()
-        if currentPed == playerPed then
+        if currentPed == PlayerPed then
             goto skip
         end
         TriggerEvent("br_utils:onPedChange")
-        playerPed = currentPed
+        PlayerPed = currentPed
         ::skip::
     end
 end)
