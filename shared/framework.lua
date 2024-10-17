e2a6053ce7df2d0d26379eff00bb01c396ff54d6
Vincent Acanfora <brodino96@gmail.com>
2024-10-17 09:04:24 +0200

    Removed empty spaces

--- shared/framework.lua
+++ shared/framework.lua
@@ -1,10 +1,7 @@
 local function getFramework()
-    if GetResourceState("es_extended") == "started" then
-        return "esx"
-    elseif GetResourceState("qb-core") == "started" then
-        return "qb"
-    elseif GetResourceState("ox_core") == "started" then
-        return "ox"
+    if GetResourceState("es_extended") == "started" then return "esx"
+    elseif GetResourceState("qb-core") == "started" then return "qb"
+    elseif GetResourceState("ox_core") == "started" then return "ox"
     end
 end
 
