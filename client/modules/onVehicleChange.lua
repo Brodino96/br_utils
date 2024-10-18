b1b80cff6e0baf4a256591a0669897e65239b5ab
Vincent Acanfora <brodino96@gmail.com>
2024-10-18 12:28:40 +0200

    "onVehicleChange" event now passes the current vehicle

--- client/modules/onVehicleChange.lua
+++ client/modules/onVehicleChange.lua
@@ -3,12 +3,11 @@ LastVehicle = GetVehiclePedIsIn(PlayerPed, false)
 CreateThread(function ()
     while true do
         Wait(0)
-
         local currentVeh = GetVehiclePedIsIn(PlayerPed, false)
         if currentVeh == LastVehicle then
             goto skip
         end
-        TriggerEvent("br_utils:onVehicleChange")
+        TriggerEvent("br_utils:onVehicleChange", currentVeh)
         LastVehicle = currentVeh
         ::skip::
     end
