1482b670290ce97236a1d0d58d44c3575f9e15af
Vincent Acanfora <brodino96@gmail.com>
2024-10-18 11:03:00 +0200

    updated the weaponDamage config to use values i think i like

--- config.lua
+++ config.lua
@@ -16,83 +16,84 @@ Config = {
     weaponDamage = { -- If active the script will override the current weapon damage with the ones in the values array
         active = true,
         values = {
+            --[[ TIER 1 ]]
             -- Pistols
-            ["WEAPON_PISTOL"] = 10.0,
-            ["WEAPON_TECPISTOL"] = 10.0,
-            ["WEAPON_APPISTOL"] = 10.0,
-            ["WEAPON_CERAMICPISTOL"] = 10.0,
-            ["WEAPON_PISTOLXM3"] = 10.0,
-            ["WEAPON_DOUBLEACTION"] = 10.0,
-            ["WEAPON_HEAVYPISTOL"] = 10.0,
-            ["WEAPON_MACHINEPISTOL"] = 10.0,
-            ["WEAPON_MARKSMANPISTOL"] = 10.0,
-            ["WEAPON_NAVYREVOLVER"] = 10.0,
-            ["WEAPON_GADGETPISTOL"] = 10.0,
-            ["WEAPON_PISTOL50"] = 10.0,
-            ["WEAPON_REVOLVER"] = 10.0,
-            ["WEAPON_SNSPISTOL"] = 10.0,
-            ["WEAPON_VINTAGEPISTOL"] = 10.0,
-            -- MK2 Pistols
-            ["WEAPON_PISTOL_MK2"] = 10.0,
-            ["WEAPON_REVOLVER_MK2"] = 10.0,
-            ["WEAPON_SNSPISTOL_MK2"] = 10.0,
-
-            -- Rifles
-            ["WEAPON_ADVANCEDRIFLE"] = 10.0,
-            ["WEAPON_ASSAULTRIFLE"] = 10.0,
-            ["WEAPON_BULLPUPRIFLE"] = 10.0,
-            ["WEAPON_CARBINERIFLE"] = 10.0,
-            ["WEAPON_BATTLERIFLE"] = 10.0,
-            ["WEAPON_COMBATPISTOL"] = 10.0,
-            ["WEAPON_COMPACTRIFLE"] = 10.0,
-            ["WEAPON_HEAVYRIFLE"] = 10.0,
-            ["WEAPON_MILITARYRIFLE"] = 10.0,
-            ["WEAPON_SPECIALCARBINE"] = 10.0,
-            ["WEAPON_TACTICALRIFLE"] = 10.0,
-            -- MK2 Rifles
-            ["WEAPON_ASSAULTRIFLE_MK2"] = 10.0,
-            ["WEAPON_BULLPUPRIFLE_MK2"] = 10.0,
-            ["WEAPON_CARBINERIFLE_MK2"] = 10.0,
-            ["WEAPON_SPECIALCARBINE_MK2"] = 10.0,
-
-            -- SMGs
-            ["WEAPON_ASSAULTSMG"] = 10.0,
-            ["WEAPON_COMBATPDW"] = 10.0,
-            ["WEAPON_GUSENBERG"] = 10.0,
-            ["WEAPON_MICROSMG"] = 10.0,
-            ["WEAPON_MINISMG"] = 10.0,
-            ["WEAPON_SMG"] = 10.0,
-            -- MK2 SMGs
-            ["WEAPON_SMG_MK2"] = 10.0,
-
+            ["WEAPON_PISTOL"] = 15.0,
+            ["WEAPON_COMBATPISTOL"] = 15.0,
+            ["WEAPON_CERAMICPISTOL"] = 15.0,
+            ["WEAPON_SNSPISTOL"] = 15.0,
+            ["WEAPON_VINTAGEPISTOL"] = 15.0,
+            ["WEAPON_PISTOLXM3"] = 15.0,
             -- Shotguns
-            ["WEAPON_ASSAULTSHOTGUN"] = 10.0,
-            ["WEAPON_BULLPUPSHOTGUN"] = 10.0,
-            ["WEAPON_COMBATSHOTGUN"] = 10.0,
-            ["WEAPON_DBSHOTGUN"] = 10.0,
-            ["WEAPON_HEAVYSHOTGUN"] = 10.0,
-            ["WEAPON_PUMPSHOTGUN"] = 10.0,
-            ["WEAPON_SAWNOFFSHOTGUN"] = 10.0,
-            ["WEAPON_AUTOSHOTGUN"] = 10.0,
-            -- MK2 Shotguns
-            ["WEAPON_PUMPSHOTGUN_MK2"] = 10.0,
+            ["WEAPON_DBSHOTGUN"] = 15.0,
+            ["WEAPON_SAWNOFFSHOTGUN"] = 15.0,
 
+            --[[ TIER 2 ]]
+            -- Pistols
+            ["WEAPON_DOUBLEACTION"] = 20.0,
+            ["WEAPON_HEAVYPISTOL"] = 20.0,
+            ["WEAPON_PISTOL50"] = 20.0,
+            ["WEAPON_PISTOL_MK2"] = 20.0,
+            ["WEAPON_SNSPISTOL_MK2"] = 20.0,
+            -- Shotguns
+            ["WEAPON_PUMPSHOTGUN"] = 20.0,
+            ["WEAPON_BULLPUPSHOTGUN"] = 20.0,
+            -- SMGs
+            ["WEAPON_SMG"] = 15.0,
+            ["WEAPON_MICROSMG"] = 15.0,
+            ["WEAPON_MINISMG"] = 15.0,
+            ["WEAPON_ASSAULTSMG"] = 15.0,
+            ["WEAPON_GUSENBERG"] = 15.0,
+            ["WEAPON_TECPISTOL"] = 15.0,
+            ["WEAPON_SMG_MK2"] = 15.0,
+            ["WEAPON_COMBATPDW"] = 15.0,
+
+            --[[ TIER 3 ]]
+            -- Pistols
+            ["WEAPON_APPISTOL"] = 10.0,
+            ["WEAPON_MACHINEPISTOL"] = 10.0,
+            ["WEAPON_NAVYREVOLVER"] = 35.0,
+            ["WEAPON_REVOLVER"] = 35.0,
+            ["WEAPON_REVOLVER_MK2"] = 35.0,
+            -- Shotguns
+            ["WEAPON_ASSAULTSHOTGUN"] = 15.0,
+            ["WEAPON_COMBATSHOTGUN"] = 15.0,
+            ["WEAPON_HEAVYSHOTGUN"] = 15.0,
+            ["WEAPON_AUTOSHOTGUN"] = 15.0,
+            ["WEAPON_PUMPSHOTGUN_MK2"] = 30.0,
+            -- Rifles
+            ["WEAPON_ADVANCEDRIFLE"] = 20.0,
+            ["WEAPON_ASSAULTRIFLE"] = 20.0,
+            ["WEAPON_BULLPUPRIFLE"] = 20.0,
+            ["WEAPON_CARBINERIFLE"] = 20.0,
+            ["WEAPON_COMPACTRIFLE"] = 20.0,
+            ["WEAPON_HEAVYRIFLE"] = 20.0,
+            ["WEAPON_MILITARYRIFLE"] = 20.0,
+            ["WEAPON_SPECIALCARBINE"] = 20.0,
+            ["WEAPON_TACTICALRIFLE"] = 20.0,
+            ["WEAPON_ASSAULTRIFLE_MK2"] = 20.0,
+            ["WEAPON_BULLPUPRIFLE_MK2"] = 20.0,
+            ["WEAPON_CARBINERIFLE_MK2"] = 20.0,
+            ["WEAPON_SPECIALCARBINE_MK2"] = 20.0,
+            ["WEAPON_BATTLERIFLE"] = 35.0,
             -- MGs
-            ["WEAPON_COMBATMG"] = 10.0,
-            ["WEAPON_MG"] = 10.0,
-            ["WEAPON_MINIGUN"] = 10.0,
-            -- MK2 MGs
-            ["WEAPON_COMBATMG_MK2"] = 10.0,
+            ["WEAPON_COMBATMG"] = 15.0,
+            ["WEAPON_MG"] = 15.0,
+            ["WEAPON_MINIGUN"] = 15.0,
+            ["WEAPON_COMBATMG_MK2"] = 15.0,
 
+            --[[ TIER SPECIAL ]]
+            -- Pistols
+            ["WEAPON_MARKSMANPISTOL"] = 300.0,
+            ["WEAPON_GADGETPISTOL"] = 300.0,
             -- Snipers
-            ["WEAPON_HEAVYSNIPER"] = 10.0,
-            ["WEAPON_MARKSMANRIFLE"] = 10.0,
-            ["WEAPON_MUSKET"] = 10.0,
-            ["WEAPON_SNIPERRIFLE"] = 10.0,
-            ["WEAPON_PRECISIONRIFLE"] = 10.0,
-            -- MK2 Snipers
-            ["WEAPON_HEAVYSNIPER_MK2"] = 10.0,
-            ["WEAPON_MARKSMANRIFLE_MK2"] = 10.0,
+            ["WEAPON_HEAVYSNIPER"] = 300.0,
+            ["WEAPON_MARKSMANRIFLE"] = 300.0,
+            ["WEAPON_MUSKET"] = 300.0,
+            ["WEAPON_SNIPERRIFLE"] = 300.0,
+            ["WEAPON_PRECISIONRIFLE"] = 300.0,
+            ["WEAPON_HEAVYSNIPER_MK2"] = 300.0,
+            ["WEAPON_MARKSMANRIFLE_MK2"] = 300.0,
         }
     }
 }
